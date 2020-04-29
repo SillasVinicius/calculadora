a52fa5b7b00be2b270b2259588cce1546df6ee16
HMSConsulting <projetos@hmsconsulting.com.br>
2020-04-29 15:42:37 -0300

    initial

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
+import { CalculadoraService } from './services';
+
+@NgModule({
+  declarations: [CalculadoraComponent],
+  imports: [CommonModule],
+  exports: [CalculadoraComponent],
+  providers: [CalculadoraService],
+})
+export class CalculadoraModule {}
---
+++ src/app/calculadora/index.ts
@@ -0,0 +1,3 @@
+export * from './calculadora.module';
+export * from './services';
+export * from './components';
